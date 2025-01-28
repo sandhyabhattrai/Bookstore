0a3777804c81d2f0ea7ce8e88ab4bcd8b9c1bcc8
sandhyabhattrai <bhattaraisandhya666@gmail.com>
2025-01-29 01:20:14 +0545

    sixth day

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .data import list_all_books
+from .models import Book
 
 # Create your views here.
 def index(request):
@@ -21,10 +22,10 @@ def contact(request):
 
 
 def all_books(request):
-    contents = {
-        'collection': list_all_books
-    } 
-    return render(request,"books/collection.html",contents)
+    allbooks = Book.objects.all()
+    return render(request,"books/collection.html",{
+        'books': allbooks
+    })
 
 def get_book_by_id(request,id):
     data ={}
@@ -35,4 +36,5 @@ def get_book_by_id(request,id):
     return render(request,"books/book_detail.html",{
         'book': data
     })
+
 
